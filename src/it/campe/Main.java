f158e18cf288160af4c063950810883532a41cb3
malyyboh <malyyboh@gmail.com>
2021-10-24 09:52:58 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -1,9 +1,17 @@
 package it.campe;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String name2 = scanner.nextLine();
+        String name3 = scanner.nextLine();
+
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name3);
 
     }
 }
